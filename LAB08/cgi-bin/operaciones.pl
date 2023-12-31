2505f739710e9723458d6cd15574a669760046ff
JuanSergioZeballos <jzeballosp@unsa.edu.pe>
2023-12-31 00:29:41 -0500

    Codigos para los demas resultados

--- LAB08/cgi-bin/operaciones.pl
+++ LAB08/cgi-bin/operaciones.pl
@@ -1,5 +1,6 @@
 #!usr/bin/perl
 use strict;
+use warnings;
 use CGI;
 my $cgi = new CGI;
 my $text = $cgi->param('operacion');
@@ -8,5 +9,26 @@ if($text=~ m/(.+)*(.+)/){
 	$num2=$2;
 	$result=$num1*$num2;
 	print $cgi->header("text/html");
+    print 'El resultado de la multiplicacion es: '.$result;
+}
+if($text=~ m/(.+)/(.+)/){
+	$num1=$1;
+	$num2=$2;
+	$result=$num1/$num2;
+	print $cgi->header("text/html");
     print 'El resultado de la division es: '.$result;
+}
+if($text=~ m/(.+)-(.+)/){
+	$num1=$1;
+	$num2=$2;
+	$result=$num1-$num2;
+	print $cgi->header("text/html");
+    print 'El resultado de la resta es: '.$result;
+}
+if($text=~ m/(.+)+(.+)/){
+	$num1=$1;
+	$num2=$2;
+	$result=$num1+$num2;
+	print $cgi->header("text/html");
+    print 'El resultado de la suma es: '.$result;
 }
